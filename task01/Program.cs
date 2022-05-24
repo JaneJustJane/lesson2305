8da2722988aecf55386856923bc9c229d8c0846a
Eugenia Galkina <yavsehmilee@gmail.com>
2022-05-24 23:44:11 +0300

    digitX to 0and1

---
+++ task01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу преобразования десятичного числа в двоичное
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int rest = a%2;
+int aa = rest;
+a = a/2;
+int i = 10;
+while(a>0)
+{
+    rest=a%2;
+    a=a/2;
+    aa=aa+rest*i;
+    i=i*10;
+}
+Console.WriteLine(aa);
